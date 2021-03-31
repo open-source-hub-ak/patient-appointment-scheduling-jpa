b3ea7230cd0a45bd3213b853b23caf766ff467ce
aakash <aakash>
2021-03-31 20:55:49 +0530

    first commit

---
+++ src/main/java/com/opensource/patientscheduling/PatientschedulingJpaApplication.java
@@ -0,0 +1,13 @@
+package com.opensource.patientscheduling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatientschedulingJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatientschedulingJpaApplication.class, args);
+	}
+
+}

096cf6fec598ed166eefdb5ed05cbd8df5a949bc
aakash <aakash>
2021-03-31 21:02:11 +0530

    Entities created

---
+++ src/main/java/com/opensource/patientscheduling/entities/Appointment.java
@@ -0,0 +1,58 @@
+package com.opensource.patientscheduling.entities;
+
+import java.sql.Timestamp;
+
+public class Appointment {
+
+	private Long id;
+	private Timestamp appointmentTime;
+	private boolean started;
+	private boolean ended;
+	private String reason;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Timestamp getAppointmentTime() {
+		return appointmentTime;
+	}
+
+	public void setAppointmentTime(Timestamp appointmentTime) {
+		this.appointmentTime = appointmentTime;
+	}
+
+	public boolean getStarted() {
+		return started;
+	}
+
+	public void setStarted(boolean started) {
+		this.started = started;
+	}
+
+	public boolean getEnded() {
+		return ended;
+	}
+
+	public void setEnded(boolean ended) {
+		this.ended = ended;
+	}
+
+	public String getReason() {
+		return reason;
+	}
+
+	public void setReason(String reason) {
+		this.reason = reason;
+	}
+
+	@Override
+	public String toString() {
+		return "Appointment [id=" + id + ", appointmentTime=" + appointmentTime + ", reason=" + reason + "]";
+	}
+
+}
